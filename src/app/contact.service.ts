22b015b02d53b843d050b2d5b3a9e5bd63b71d53
krishnarao punugoti <krishna@fancyfox.in>
2022-02-05 14:56:08 +0530

    stable commit

--- src/app/contact.service.ts
+++ src/app/contact.service.ts
@@ -6,9 +6,8 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root'
 })
 export class ContactService {
-      myForm: any; 
-  contactList : Contact[]=[];
-  constructor(private http: HttpClient) { }
+     
+  constructor() { }
   getData(){
     return [
         {"id":1, "name":"uddav", "mobile":1234567830},
@@ -17,22 +16,12 @@ export class ContactService {
         {"id":4, "name":"patnaik", "mobile":1234567845},
     ];
   }
-  getContacts(){
-    return this.contactList;
-  }
-  getContact(id: string){
-    let contact: Contact;
-    this.contactList.map(val=>{
-      if(val.id == id) contact = val;
-    });
-    
-  }
-  contactEdit(contact:any){
+  
+}
+/*contactEdit(contact:any){
     let present: Boolean = false;
     this.contactList.map((val, index)=>{
       if(val.id == contact.id) {this.contactList[index] = contact;present=true}
     });
     return present;
-
-  }
-}
+  }*/ 
--- src/app/contactadd/contactadd.component.ts
+++ src/app/contactadd/contactadd.component.ts
@@ -9,62 +9,9 @@ import { Contact } from '../contact-interface';
   templateUrl: './contactadd.component.html',
   styleUrls: ['./contactadd.component.css']
 })
-export class ContactaddComponent implements OnInit {
+export class ContactaddComponent {
+ 
+  constructor(private contactService: ContactService) { }
+ 
 
-  msg:string ='';
-  myForm!: FormGroup;
-  isEdit: Boolean = false;
-
-  constructor(
-    private contactService: ContactService,
-    private route : ActivatedRoute,
-    private router : Router
-    ) { }
-
-  ngOnInit(): void {
-
-    this.myForm = new FormGroup({
-      name: new FormControl(''),
-      id: new FormControl(''),
-      mobile: new FormControl(''),
-    })
-      this.route.params.subscribe(param => {
-        console.log(param)
-        if(param ){
-          let student = this.contactService.getData();
-          if(student){
-            this.myForm.setValue(student);
-            this.isEdit = true;
-            }
-          else this.router.navigate(['/students'])
-        }
-      })
-  }
-  onSubmit(){
-    localStorage.setItem("formdata",JSON.stringify(this.myForm.value));
-  }
-  resetForm(){
-    this.myForm.reset();
-  }
-  add(){
-    if(this.myForm.valid){
-      this.contactService.contactList.push(this.myForm.value);
-      this.resetForm();
-    }
-      else {
-        this.msg = 'Please complete form'
-      }
-  }
-  edit(){
-    this.msg = '';
-    if(this.myForm.valid){
-      if(this.contactService.contactEdit(this.myForm.value)){
-        this.router.navigate(['/students'])
-      }else {
-        this.msg = 'Something went wrong'
-      }
-    }else {
-      this.msg = 'Please complete form'
-    }
-  } 
 }
